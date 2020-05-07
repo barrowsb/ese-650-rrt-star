cd23167164d35588b3688fb32eccc668f1d23491
Tutch Winyarat <sw3113@columbia.edu>
2020-05-07 11:52:50 -0400

    Added near-path sampling method

--- utils.py
+++ utils.py
@@ -1,10 +1,19 @@
 import numpy as np
+import random
 
 
 def sampleUniform(xmin, ymin, xmax, ymax):
 	return np.array([np.random.uniform(xmin, xmax), np.random.uniform(ymin, ymax) ])
 
 def sampleGaussian(mean, covar):
+	return np.random.multivariate_normal(mean, covar)
+
+def sampleNearPath(path, covar = 2.0*np.eye(2)):
+	#draws from Gaussian centered at randomly interpolated wayPt on path
+	numWayPts = np.shape(path)[0]
+	randID = np.random.randint(0, numWayPts-2)
+	alpha = random.random()
+	mean = alpha*path[randID] + (1-alpha)*path[randID+1] #interpolate
 	return np.random.multivariate_normal(mean, covar)
 
 def steer(eta,qnear, qrand):
