686b4980e6e6776ed0ea885dcee5d63bff9e392a
Tutch Winyarat <sw3113@columbia.edu>
2020-05-10 19:51:53 -0400

    Updated regrow(), moved drawing functs to utils.py, cleaned up rrt_star_FND

--- rrt_star_FND.py
+++ rrt_star_FND.py
@@ -14,8 +14,9 @@ start = [0,0]
 goal = [10,10]
 xmin, ymin, xmax, ymax = -15,-15,15,15 #grid world borders
 obst1 = Obstacle('rect',[3, 3, 3,3], [0,0], np.eye(2))
-obst2 = Obstacle('circle',[2,-2,1], [0,0], np.eye(2))
-obstacles = [obst1, obst2] #list of obstacles
+obst2 = Obstacle('circle',[2,5,2], [0,0], np.eye(2))
+obst3 = Obstacle('rect', [8,-4,1,4], [0,0], np.eye(2))
+obstacles = [obst1, obst2, obst3] #list of obstacles
 N = 2000 #number of iterations
 epsilon = 0.5 #near goal tolerance
 maxNumNodes = 1000 #upper limit on tree size 
@@ -24,22 +25,14 @@ gamma = 20.0 #param to set for radius of hyperball
 goalFound = False
 #########################################
 #for plotting
-iterations = []
-costs = []
-path = [];
-
-
-######################################
-#### Helper Methods ##################
-######################################
-def takeSnapShot(tree):
-	pass
-
-
-
-
-
-
+# iterations = []
+# costs = []
+# path = [];
+fig, ax = plt.subplots()
+plt.ylim((-15,15))
+plt.xlim((-15,15))
+ax.set_aspect('equal', adjustable='box')
+#########################################
 
 
 #########################################
@@ -52,12 +45,12 @@ tree = Tree(start, goal, obstacles, xmin,ymin,xmax, ymax)
 #3. Get Solution Path
 solPath, solPathID = tree.initGrowth()
 ####################
-#4. Init movement()-->> update pcurID
+#4. Init movement()-->> update pcurID 
 tree.pcurID = solPathID[1]
 ####################
-#5. Begin replanning loop, while pcur is not goal, do...
+#5. Begin replanning loop (and move obstacles?), while pcur is not goal, do...
 # while np.linagl.norm(tree.nodes[tree.pcurID, 0:2] - goal) > epsilon:
-for __ in range(5):
+for __ in range(1):
 	#6. Obstacle Updates
 	# tree.updateObstacles()
 	tree.obstacles[0].position = np.array([ 7, 2 ])
@@ -66,72 +59,43 @@ for __ in range(5):
 		#8. Stop Movement
 		#9. select remaining valid branches
 		solPathID = tree.selectBranch(tree.pcurID, solPathID)
-		print("path breaks, collision detected!")
+		print("********************************************************")
+		print("********* Path Breaks, collision detected! *************")
+		print("********************************************************")
 		# break
-		separatePathID, orphanedTree, deadNodes = tree.validPath(solPathID)
+		#10. Separate tree
+		separatePathID, orphanedTree = tree.validPath(solPathID)
+		separatePath = orphanedTree[separatePathID, 0:2].reshape(-1,2)
+		#.11-20 Try to reconnect main with orphanedTree
+		reconnected = tree.reconnect(separatePathID)
+
+		if reconnected:
+			print('\n')
+			print("				RECONNECT SUCCESSFUL !		")
+			print('\n')
+			utils.drawTree(tree.nodes, ax, 'blue')
 		
+		else:
+			print('\n')
+			print("				RECONNECT FAILED!		")
+			print('\n')
+			#21. if reconnect fails, regrow tree
+			solPath, solPathID = tree.regrow()
 
+			print('\n')
+			print("				REGROW SUCCESSFUL !		")
+			print('\n')
+			utils.drawTree(tree.nodes, ax, 'grey')
+		
+		utils.drawTree(orphanedTree, ax, 'purple')
+		utils.drawPath(separatePath, ax, 'green')
 
-
-
-
-
-
-
-
-
-
-
-
-
-#########################################
-############### Draw tree ###############
-#########################################
-def draw_edge(a, b, ax, color = 'blue'):
-    path = Path([(a[0], a[1]), (b[0], b[1])], [Path.MOVETO, Path.LINETO])
-    pathpatch = patches.PathPatch(path, facecolor='white', edgecolor= color)
-    ax.add_patch(pathpatch)
-    # plt.pause(10**-9)
-
-#########################################
-########## Plot final FN tree ###########
-#########################################
-fig, ax = plt.subplots()
-plt.ylim((-15,15))
-plt.xlim((-15,15))
-ax.set_aspect('equal', adjustable='box')
-
-
-def draw_edge(a, b, ax, color = 'blue'):
-    path = Path([(a[0], a[1]), (b[0], b[1])], [Path.MOVETO, Path.LINETO])
-    pathpatch = patches.PathPatch(path, facecolor='white', edgecolor= color)
-    ax.add_patch(pathpatch)
-
-# for i in range(np.shape(spath)[0]-1):
-# 	draw_edge(spath[i], spath[i+1], ax, 'green')
-
-for i in range(np.shape(tree.nodes)[0]):
-	if tree.nodes[i, 3] != -1:
-		parentID = int(tree.nodes[i, 3])
-		draw_edge(tree.nodes[i, 0:2], tree.nodes[parentID, 0:2], ax)
-
-
-ax.add_patch(obst1.toPatch())
-ax.add_patch(obst2.toPatch())
-ax.add_patch(patches.Circle((start[0], start[1]), 0.5, facecolor = 'red' ) )
-ax.add_patch(patches.Circle((goal[0], goal[1]), 0.5, facecolor = 'yellow' ))
-
-
-
-path = tree.nodes[solPathID, 0:2].reshape(-1,2)
-for i in range(np.shape(path)[0]-1):
-	draw_edge(path[i, :], path[i+1, :], ax, 'green')
-
-# #########################################
-# ############### Plot cost ###############
-# #########################################
-# plt.figure(2)
-# plt.title('Cost to reach goal vs Number of Iterations')
-# plt.plot(iterations, costs)
+#### DRAW ENVIRONMENT ######
+for obs in tree.obstacles:
+	utils.drawShape(obs.toPatch(), ax)
+G = Obstacle('circle', [goal[0], goal[1], 0.3], [0,0], 0*np.eye(2))
+S = Obstacle('circle', [start[0], start[1], 0.3], [0,0], 0*np.eye(2))
+utils.drawShape(G.toPatch('red'), ax)
+utils.drawShape(S.toPatch('pink'), ax)
 
 plt.show()
