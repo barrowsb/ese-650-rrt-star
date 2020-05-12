51631d28c6e560a183d82201dff054e1c192fd06
Tutch Winyarat <sw3113@columbia.edu>
2020-05-12 11:03:29 -0400

    nextSolNode(): Computes dt

--- Tree.py
+++ Tree.py
@@ -555,7 +555,7 @@ class Tree(object):
 	def nextSolNode(self,solPath,solPathID):
 		#update pcur to the next sol node and return shortened solpathID
 		self.pcurID = solPathID[1]
-		dt = 1 #TODO: fill this in
+		dt = self.nodes[solPathID[1], 2] - self.nodes[solPathID[0], 2]
 		return solPath[1:],solPathID[1:],dt
 
 	def reset(self, inheritCost = True):
